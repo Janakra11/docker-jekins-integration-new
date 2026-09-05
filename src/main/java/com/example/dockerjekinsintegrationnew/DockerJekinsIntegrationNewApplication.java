aa823e231b9f74527e6fe105828d0148d9419154
Janakra11 <janakrajkhatal@gmail.com>
2026-09-06 00:39:04 +0530

    first commit

---
+++ src/main/java/com/example/dockerjekinsintegrationnew/DockerJekinsIntegrationNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerjekinsintegrationnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJekinsIntegrationNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerJekinsIntegrationNewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dockerjekinsintegrationnew/DockerJekinsIntegrationNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerjekinsintegrationnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJekinsIntegrationNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
